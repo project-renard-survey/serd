c8540c3a95ccd4d154153dc3885b426c34d0a64d
David Robillard <d@drobilla.net>
2020-04-28 00:03:32 +0200

    fixup! WIP: Add Python bindings

--- bindings/python/serd.py
+++ bindings/python/serd.py
@@ -1,13 +1,13 @@
 """Serd Python interface"""
 
 import sys
-from enum import Enum
 
-from ctypes import Structure, CDLL, POINTER, CFUNCTYPE
+from enum import IntEnum
+
+from ctypes import Structure, CDLL, POINTER
 from ctypes import c_bool, c_double, c_float, c_int, c_uint
-from ctypes import c_size_t, c_uint32, c_int64
-from ctypes import c_char, c_char_p, c_void_p
-from ctypes import byref, cast
+from ctypes import c_size_t, c_int64
+from ctypes import c_char_p, c_void_p
 
 
 class _SerdLib:
@@ -26,7 +26,7 @@ class _SerdLib:
 c = _SerdLib()
 
 
-class Status(Enum):
+class Status(IntEnum):
     SUCCESS = 0  # No error
     FAILURE = 1  # Non-fatal failure
     ERR_UNKNOWN = 2  # Unknown error
@@ -43,7 +43,7 @@ class Status(Enum):
     ERR_BAD_WRITE = 13  # Error writing to file/stream
 
 
-class Syntax(Enum):
+class Syntax(IntEnum):
     EMPTY = 0  # Empty syntax (suppress input or output)
     TURTLE = 1  # Terse triples http://www.w3.org/TR/turtle
     NTRIPLES = 2  # Flat triples http://www.w3.org/TR/n-triples/
@@ -51,7 +51,7 @@ class Syntax(Enum):
     TRIG = 4  # Terse quads http://www.w3.org/TR/trig/
 
 
-class StatementFlag(Enum):
+class StatementFlag(IntEnum):
     EMPTY_S = 1 << 0  # Empty blank node subject
     ANON_S = 1 << 1  # Start of anonymous subject
     ANON_O = 1 << 2  # Start of anonymous object
@@ -61,11 +61,11 @@ class StatementFlag(Enum):
     TERSE_O = 1 << 6  # Terse serialisation of new object
 
 
-class SerialisationFlag(Enum):
+class SerialisationFlag(IntEnum):
     NO_INLINE_OBJECTS = 1 << 0  # Disable object inlining
 
 
-class NodeType(Enum):
+class NodeType(IntEnum):
     LITERAL = 1  # Literal value
     URI = 2  # URI (absolute or relative)
     CURIE = 3  # CURIE (shortened URI)
@@ -73,21 +73,21 @@ class NodeType(Enum):
     VARIABLE = 5  # Variable node
 
 
-class NodeFlag(Enum):
+class NodeFlag(IntEnum):
     HAS_NEWLINE = 1  # Contains line breaks ('\\n' or '\\r')
     HAS_QUOTE = 1 << 1  # Contains quotes ('"')
     HAS_DATATYPE = 1 << 2  # Literal node has datatype
     HAS_LANGUAGE = 1 << 3  # Literal node has language
 
 
-class Field(Enum):
+class Field(IntEnum):
     SUBJECT = 0  # Subject
     PREDICATE = 1  # Predicate ("key")
     OBJECT = 2  # Object ("value")
     GRAPH = 3  # Graph ("context")
 
 
-class ModelFlag(Enum):
+class ModelFlag(IntEnum):
     INDEX_SPO = 1 << 0  # Subject,   Predicate, Object
     INDEX_SOP = 1 << 1  # Subject,   Object,    Predicate
     INDEX_OPS = 1 << 2  # Object,    Predicate, Subject
@@ -101,12 +101,12 @@ class ModelFlag(Enum):
 # TODO: URI
 
 
-class ReaderFlag(Enum):
+class ReaderFlag(IntEnum):
     READ_LAX = 1 << 0  # Tolerate invalid input where possible
     READ_VARIABLES = 1 << 1  # Support variable nodes
 
 
-class WriterFlag(Enum):
+class WriterFlag(IntEnum):
     WRITE_ASCII = 1 << 0  # Escape all non-ASCII characters
     WRITE_TERSE = 1 << 1  # Write terser output without newlines
     WRITE_LAX = 1 << 2  # Tolerate lossy output
@@ -130,15 +130,84 @@ class Nodes(Structure):
 class Node(Structure):
     @classmethod
     def wrap(cls, node):
+        assert type(node) == P(Node)
         return Node(node) if node else None
 
     @classmethod
     def string(cls, s):
         return Node.wrap(c.new_string(s))
 
+    @classmethod
+    def plain_literal(cls, s, lang):
+        return Node.wrap(c.new_plain_literal(s, lang))
+
+    @classmethod
+    def typed_literal(cls, s, datatype):
+        return Node.wrap(c.new_typed_literal(s, datatype.node))
+
+    @classmethod
+    def blank(cls, s):
+        return Node.wrap(c.new_blank(s))
+
+    @classmethod
+    def curie(cls, s):
+        return Node.wrap(c.new_curie(s))
+
+    @classmethod
+    def uri(cls, s):
+        return Node.wrap(c.new_uri(s))
+
+    @classmethod
+    def resolved_uri(cls, s, base):
+        return Node.wrap(c.new_resolved_uri(s, base.node))
+
+    @classmethod
+    def file_uri(cls, path, hostname):
+        return Node.wrap(c.new_file_uri(path, hostname))
+
+    @classmethod
+    def relative_uri(cls, s, base, root):
+        return Node.wrap(c.new_relative_uri(s, base, root))
+
+    @classmethod
+    def decimal(cls, d, max_precision, max_frac_digits, datatype):
+        return Node.wrap(
+            c.new_decimal(d, max_precision, max_frac_digits, datatype)
+        )
+
+    @classmethod
+    def double(cls, d):
+        return Node.wrap(c.new_double(d))
+
+    @classmethod
+    def float(cls, f):
+        return Node.wrap(c.new_float(f))
+
+    @classmethod
+    def integer(cls, i, datatype=None):
+        return Node.wrap(c.new_integer(i, datatype.node if datatype else None))
+
+    @classmethod
+    def boolean(cls, b):
+        return Node.wrap(c.new_boolean(b))
+
+    @classmethod
+    def blob(cls, buf, wrap_lines=False, datatype=None):
+        assert type(wrap_lines) == bool
+        assert datatype is None or type(datatype) == Node
+        return Node.wrap(
+            c.new_blob(
+                buf, len(buf), wrap_lines, datatype.node if datatype else None
+            )
+        )
+
     def __init__(self, node):
         assert type(node) == POINTER(Node)
         self.node = node
+
+    def __del(self):
+        c.node_free(self.node)
+        self.node = None
 
     def __str__(self):
         return c.node_get_string(self.node).decode("utf-8")
@@ -146,8 +215,18 @@ class Node(Structure):
     def __len__(self):
         return c.node_get_length(self.node)
 
+    def __eq__(self, rhs):
+        if rhs is None:
+            return False
+
+        rhs_type = type(rhs)
+        if rhs_type == Node:
+            return c.node_equals(self.node, rhs.node)
+
+        return rhs_type(self) == rhs
+
     def type(self):
-        return c.node_get_type(self.node)
+        return NodeType(c.node_get_type(self.node))
 
     def datatype(self):
         return Node.wrap(c.node_copy(c.node_get_datatype(self.node)))
@@ -212,31 +291,26 @@ _cfunc("world_get_blank", P(World))
 
 _cfunc("new_string", P(Node), String)
 # new_substring
+# new_literal
+_cfunc("new_plain_literal", P(Node), String, String)
+_cfunc("new_typed_literal", P(Node), String, P(Node))
+_cfunc("new_blank", P(Node), String)
+_cfunc("new_curie", P(Node), String)
 
-_cfunc(
-    "new_literal",
-    P(Node),
-    c_char_p,
-    c_size_t,
-    c_char_p,
-    c_size_t,
-    c_char_p,
-    c_size_t,
-)
-
-_cfunc("new_plain_literal", P(Node), c_char_p, c_char_p)
-_cfunc("new_typed_literal", P(Node), c_char_p, P(Node))
-_cfunc("new_blank", P(Node), c_char_p)
-_cfunc("new_curie", P(Node), c_char_p)
+# TODO: Fix order in serd.h
 _cfunc("node_copy", P(Node), P(Node))
 _cfunc("node_equals", c_bool, P(Node), P(Node))
 _cfunc("node_compare", c_int, P(Node), P(Node))
-_cfunc("new_uri", P(Node), c_char_p)
-_cfunc("new_resolved_uri", P(Node), c_char_p, P(Node))
+
+_cfunc("new_uri", P(Node), String)
+_cfunc("new_resolved_uri", P(Node), String, P(Node))
 # _cfunc("node_normalise", P(Node), P(Env), P(Node))
+
+# TODO: Fix order in serd.h
 _cfunc("node_resolve", P(Node), P(Node))
-_cfunc("new_file_uri", P(Node), c_char_p, c_char_p)
-_cfunc("new_relative_uri", P(Node), c_char_p, P(Node), P(Node))
+
+_cfunc("new_file_uri", P(Node), String, String)
+_cfunc("new_relative_uri", P(Node), String, P(Node), P(Node))
 _cfunc("new_decimal", P(Node), c_double, c_uint, c_uint, P(Node))
 _cfunc("new_double", P(Node), c_double)
 _cfunc("new_float", P(Node), c_float)
--- bindings/python/test_serd.py
+++ bindings/python/test_serd.py
@@ -14,6 +14,8 @@
 
 import serd
 import unittest
+import base64
+
 
 class NodeTests(unittest.TestCase):
     def setUp(self):
@@ -21,8 +23,127 @@ class NodeTests(unittest.TestCase):
 
     def testString(self):
         n = serd.Node.string("hello")
-        self.assertEqual(str(n), "hello")
+        self.assertEqual(n.type(), serd.NodeType.LITERAL)
+        self.assertEqual(n, "hello")
         self.assertEqual(len(n), 5)
         self.assertIsNone(n.datatype())
         self.assertIsNone(n.language())
         self.assertEqual(n.flags(), 0)
+
+    def testPlainLiteral(self):
+        n = serd.Node.plain_literal("hallo", "de")
+        self.assertEqual(n.type(), serd.NodeType.LITERAL)
+        self.assertEqual(n, "hallo")
+        self.assertEqual(len(n), 5)
+        self.assertIsNone(n.datatype())
+        self.assertEqual(n.language(), serd.Node.string("de"))
+        self.assertEqual(n.flags(), serd.NodeFlag.HAS_LANGUAGE)
+
+    def testTypedLiteral(self):
+        datatype = serd.Node.uri("http://example.org/ns#Hex")
+        n = serd.Node.typed_literal("ABCD", datatype)
+        self.assertEqual(n.type(), serd.NodeType.LITERAL)
+        self.assertEqual(n, "ABCD")
+        self.assertEqual(len(n), 4)
+        self.assertEqual(n.datatype(), datatype)
+        self.assertIsNone(n.language())
+        self.assertEqual(n.flags(), serd.NodeFlag.HAS_DATATYPE)
+
+    def testBlank(self):
+        n = serd.Node.blank("b0")
+        self.assertEqual(n.type(), serd.NodeType.BLANK)
+        self.assertEqual(n, "b0")
+        self.assertEqual(len(n), 2)
+        self.assertIsNone(n.datatype())
+        self.assertIsNone(n.language())
+        self.assertEqual(n.flags(), 0)
+
+    def testCurie(self):
+        n = serd.Node.curie("ns:name")
+        self.assertEqual(n.type(), serd.NodeType.CURIE)
+        self.assertEqual(n, "ns:name")
+        self.assertEqual(len(n), 7)
+        self.assertIsNone(n.datatype())
+        self.assertIsNone(n.language())
+        self.assertEqual(n.flags(), 0)
+
+    def testUri(self):
+        n = serd.Node.uri("http://example.org/")
+        self.assertEqual(n.type(), serd.NodeType.URI)
+        self.assertEqual(n, "http://example.org/")
+        self.assertEqual(len(n), 19)
+        self.assertIsNone(n.datatype())
+        self.assertIsNone(n.language())
+        self.assertEqual(n.flags(), 0)
+
+    def testDouble(self):
+        xsd_double = "http://www.w3.org/2001/XMLSchema#double"
+        n = serd.Node.double(12.34)
+        self.assertEqual(n.type(), serd.NodeType.LITERAL)
+        self.assertEqual(n, "1.234E1")
+        self.assertEqual(len(n), 7)
+        self.assertEqual(n.datatype(), serd.Node.uri(xsd_double))
+        self.assertIsNone(n.language())
+        self.assertEqual(n.flags(), serd.NodeFlag.HAS_DATATYPE)
+
+    def testFloat(self):
+        xsd_float = "http://www.w3.org/2001/XMLSchema#float"
+        n = serd.Node.float(234.5)
+        self.assertEqual(n.type(), serd.NodeType.LITERAL)
+        self.assertEqual(n, "2.345E2")
+        self.assertEqual(len(n), 7)
+        self.assertEqual(n.datatype(), serd.Node.uri(xsd_float))
+        self.assertIsNone(n.language())
+        self.assertEqual(n.flags(), serd.NodeFlag.HAS_DATATYPE)
+
+    def testInteger(self):
+        xsd_integer = "http://www.w3.org/2001/XMLSchema#integer"
+        n = serd.Node.integer(42)
+        self.assertEqual(n.type(), serd.NodeType.LITERAL)
+        self.assertEqual(n, "42")
+        self.assertEqual(len(n), 2)
+        self.assertEqual(n.datatype(), serd.Node.uri(xsd_integer))
+        self.assertIsNone(n.language())
+        self.assertEqual(n.flags(), serd.NodeFlag.HAS_DATATYPE)
+
+        xsd_decimal = "http://www.w3.org/2001/XMLSchema#decimal"
+        d = serd.Node.integer(53, serd.Node.uri(xsd_decimal))
+        self.assertEqual(d.datatype(), serd.Node.uri(xsd_decimal))
+
+    def testBoolean(self):
+        xsd_boolean = "http://www.w3.org/2001/XMLSchema#boolean"
+
+        t = serd.Node.boolean(True)
+        self.assertEqual(t.type(), serd.NodeType.LITERAL)
+        self.assertEqual(str(t), "true")
+        self.assertEqual(len(t), 4)
+        self.assertEqual(t.datatype(), serd.Node.uri(xsd_boolean))
+        self.assertIsNone(t.language())
+        self.assertEqual(t.flags(), serd.NodeFlag.HAS_DATATYPE)
+
+        f = serd.Node.boolean(False)
+        self.assertEqual(f.type(), serd.NodeType.LITERAL)
+        self.assertEqual(str(f), "false")
+        self.assertEqual(len(f), 5)
+        self.assertEqual(f.datatype(), serd.Node.uri(xsd_boolean))
+        self.assertIsNone(f.language())
+        self.assertEqual(f.flags(), serd.NodeFlag.HAS_DATATYPE)
+
+    def testBlob(self):
+        xsd_base64Binary = "http://www.w3.org/2001/XMLSchema#base64Binary"
+        n = serd.Node.blob(b"DEAD")
+        self.assertEqual(n.type(), serd.NodeType.LITERAL)
+        self.assertEqual(bytes(str(n), "utf-8"), base64.b64encode(b"DEAD"))
+        self.assertEqual(len(n), 8)
+        self.assertEqual(n.datatype(), serd.Node.uri(xsd_base64Binary))
+        self.assertIsNone(n.language())
+        self.assertEqual(n.flags(), serd.NodeFlag.HAS_DATATYPE)
+
+        datatype = "http://example.org/ns#Blob"
+        t = serd.Node.blob(b"BEEF", datatype=serd.Node.uri(datatype))
+        self.assertEqual(t.type(), serd.NodeType.LITERAL)
+        self.assertEqual(bytes(str(t), "utf-8"), base64.b64encode(b"BEEF"))
+        self.assertEqual(len(t), 8)
+        self.assertEqual(t.datatype(), serd.Node.uri(datatype))
+        self.assertIsNone(t.language())
+        self.assertEqual(t.flags(), serd.NodeFlag.HAS_DATATYPE)
